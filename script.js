e6dab6ec9243081d9d37773fc272711adf787853
zw-zack <zhengwin94@hotmail.com>
2018-03-13 10:36:16 +0800

    added cakes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function hideStartPage() {
 	document.getElementsByClassName("startPage")[0].style.display = "none";
 }
@@ -14,9 +13,6 @@ function whenStartButtonClicked() {
 	gameStart();
 }
 
-function gameStart(){
-	document.getElementsByClassName("gameStarted")[0].style.display = "block";
-}
 
 var myGamePiece;
 var myBackground;
@@ -26,12 +22,11 @@ var dinos = new Array(0);
 var ghosts = new Array(0);
 var cakes = new Array(0);
 
-		
 var myScore;
 
-
 function gameStart() {
-	myGamePiece = new component(30, 30, "cat_image.png", 580, 300, "image");
+	document.getElementsByClassName("gameStarted")[0].style.display = "block";
+	myGamePiece = new component(30, 30, "cat_image.png", 580, 300, "image", 1);
 	myBackground = new component(1200, 700, "bgImage3.jpg", 0, 0, "image");
 	myScore = new component("30px", "Consolas", "black", 1000, 40, "text");
 	var time = 0;
@@ -48,32 +43,30 @@ function gameStart() {
 	setInterval(timer, 1000);
 
 	function makeTigers(){
-		var myTiger = new enemyTypeOne(50, 50, "tiger.png", 30, 650*Math.random(), "image");
+		var myTiger = new enemyTypeOne(50, 50, "tiger.png", 30, 650*Math.random(), "image", 1);
 		tigers.push(myTiger);
 	}
 	var tigerInterval = setInterval(function(){ makeTigers()} , 3000);
 
-
 	function makeDinos(){
-		var myDino = new enemyTypeTwo(70, 70, "dino.png", 1100, 650*Math.random(), "image");
+		var myDino = new enemyTypeTwo(70, 70, "dino.png", 1100, 650*Math.random(), "image", 1);
 		dinos.push(myDino);
 	}
 	var dinoInterval = setInterval(makeDinos, 10000);
 
 	function makeGhosts(){
 		for(var i = 0; i<3 ; i++){
-			var myGhost = new enemyTypeThree(60, 60, "ghost.png", 30, 650*Math.random(), "image");
+			var myGhost = new enemyTypeThree(60, 60, "ghost.png", 30, 650*Math.random(), "image", 1);
 			ghosts.push(myGhost);
 		}
 	}
 	setTimeout(makeGhosts, 20000);
 
 	function makeCake(){
-		var myCake = new friendliesTypeOne(20, 20, "cake.jpg", Math.random()*1100 , Math.random()* 550, "image");
+		var myCake = new friendliesTypeOne(20, 20, "cake.jpg", Math.random()*1100 , Math.random()* 550, "image", 1);
 		cakes.push(myCake);
 	}
-
-	var cakeInterval = setInterval(makeCake, 10000);
+	var cakeInterval = setInterval(makeCake, 5000);
 	myGameArea.start();
 }
 
@@ -84,7 +77,7 @@ var myGameArea = {
 		this.canvas.width = 1200;
 		this.canvas.height = 700;
 		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		document.getElementsByClassName("gameStarted")[0].insertBefore(this.canvas, document.getElementsByClassName("gameStarted")[0].childNodes[0]);
 		this.frameNo = 0;
 		this.interval = setInterval(updateGameArea, 20);
 		window.addEventListener('keydown', function (e) {
@@ -104,7 +97,8 @@ var myGameArea = {
 	}
 }
 
-function component(width, height, color, x, y, type) {
+function component(width, height, color, x, y, type,status) {
+	this.status= status;
 	this.type = type;
 	if (type == "image") {
 		this.image = new Image();
@@ -155,253 +149,262 @@ function component(width, height, color, x, y, type) {
 			mytop > otherbottom ||
 			myright < otherleft ||
 			myleft > otherright) {
-				crash = false;
-			}
-			return crash;
-		}
+			crash = false;
 	}
+	return crash;
+}
+}
 
 //tigers
-	function enemyTypeOne(width, height, color, x, y, type) {
-		this.type = type;
-		if (type == "image") {
-			this.image = new Image();
-			this.image.src = color;
-		}
-		this.width = width;
-		this.height = height;
-		this.speedX = Math.random();
-		this.speedY = -Math.random();    
-		this.x = x;
-		this.y = y;    
-		this.update = function() {
-			ctx = myGameArea.context;
-			if (this.type == "image") {
-				ctx.drawImage(this.image, 
-					this.x, 
-					this.y,
-					this.width, this.height);
-			} 
-			else {
-				ctx.fillStyle = color;
-				ctx.fillRect(this.x, this.y, this.width, this.height);
-			}
-		}
-		this.newPos = function() {
-			this.x += this.speedX;
-			this.y += this.speedY;   
+function enemyTypeOne(width, height, color, x, y, type,status) {
+	this.status= status;
+	this.type = type;
+	if (type == "image") {
+		this.image = new Image();
+		this.image.src = color;
+	}
+	this.width = width;
+	this.height = height;
+	this.speedX = Math.random();
+	this.speedY = -Math.random();    
+	this.x = x;
+	this.y = y;    
+	this.update = function() {
+		ctx = myGameArea.context;
+		if (this.type == "image") {
+			ctx.drawImage(this.image, 
+				this.x, 
+				this.y,
+				this.width, this.height);
+		} 
+		else {
+			ctx.fillStyle = color;
+			ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
+	this.newPos = function() {
+		this.x += this.speedX;
+		this.y += this.speedY;   
+	}
+}
 
 //dinos
-	function enemyTypeTwo(width, height, color, x, y, type) {
-		this.type = type;
-		if (type == "image") {
-			this.image = new Image();
-			this.image.src = color;
-		}
-		this.width = width;
-		this.height = height;
-		this.speedX = Math.random() +1;
-		this.speedY = -Math.random() -1;    
-		this.x = x;
-		this.y = y;    
-		this.update = function() {
-			ctx = myGameArea.context;
-			if (this.type == "image") {
-				ctx.drawImage(this.image, 
-					this.x, 
-					this.y,
-					this.width, this.height);
-			} 
-			else {
-				ctx.fillStyle = color;
-				ctx.fillRect(this.x, this.y, this.width, this.height);
-			}
-		}
-		this.newPos = function() {
-			this.x += this.speedX;
-			this.y += this.speedY;   
+function enemyTypeTwo(width, height, color, x, y, type, status) {
+	this.status= status;
+	this.type = type;
+	if (type == "image") {
+		this.image = new Image();
+		this.image.src = color;
+	}
+	this.width = width;
+	this.height = height;
+	this.speedX = Math.random() +1;
+	this.speedY = -Math.random() -1;    
+	this.x = x;
+	this.y = y;    
+	this.update = function() {
+		ctx = myGameArea.context;
+		if (this.type == "image") {
+			ctx.drawImage(this.image, 
+				this.x, 
+				this.y,
+				this.width, this.height);
+		} 
+		else {
+			ctx.fillStyle = color;
+			ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
+	this.newPos = function() {
+		this.x += this.speedX;
+		this.y += this.speedY;   
+	}
+}
 
 //ghosts
-		function enemyTypeThree(width, height, color, x, y, type) {
-		this.type = type;
-		if (type == "image") {
-			this.image = new Image();
-			this.image.src = color;
-		}
-		this.width = width;
-		this.height = height;
-		this.speedX = Math.random() +3;
-		this.speedY = -Math.random() -3;    
-		this.x = x;
-		this.y = y;    
-		this.update = function() {
-			ctx = myGameArea.context;
-			if (this.type == "image") {
-				ctx.drawImage(this.image, 
-					this.x, 
-					this.y,
-					this.width, this.height);
-			} 
-			else {
-				ctx.fillStyle = color;
-				ctx.fillRect(this.x, this.y, this.width, this.height);
-			}
-		}
-		this.newPos = function() {
-			this.x += this.speedX;
-			this.y += this.speedY;   
+function enemyTypeThree(width, height, color, x, y, type, status) {
+	this.status= status;
+	this.type = type;
+	if (type == "image") {
+		this.image = new Image();
+		this.image.src = color;
+	}
+	this.width = width;
+	this.height = height;
+	this.speedX = Math.random() +3;
+	this.speedY = -Math.random() -3;    
+	this.x = x;
+	this.y = y;    
+	this.update = function() {
+		ctx = myGameArea.context;
+		if (this.type == "image") {
+			ctx.drawImage(this.image, 
+				this.x, 
+				this.y,
+				this.width, this.height);
+		} 
+		else {
+			ctx.fillStyle = color;
+			ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
+	this.newPos = function() {
+		this.x += this.speedX;
+		this.y += this.speedY;   
+	}
+}
 
 //cake
-	function friendliesTypeOne(width, height, color, x, y, type) {
-		this.type = type;
-		if (type == "image") {
-			this.image = new Image();
-			this.image.src = color;
-		}
-		this.width = width;
-		this.height = height;
-		this.speedX = 0;
-		this.speedY = 0;
-		this.x = x;
-		this.y = y;
-		this.update = function() {
-			ctx = myGameArea.context;
-			if (this.type == "image") {
-				ctx.drawImage(this.image, 
-					this.x, 
-					this.y,
-					this.width, this.height);
-			} 
-			else {
-				ctx.fillStyle = color;
-				ctx.fillRect(this.x, this.y, this.width, this.height);
-			}
-		}
-		this.newPos = function() {
-			this.x += this.speedX;
-			this.y += this.speedY;   
+function friendliesTypeOne(width, height, color, x, y, type, status) {
+	this.status = status;
+	this.type = type;
+	if (type == "image") {
+		this.image = new Image();
+		this.image.src = color;
+	}
+	this.width = width;
+	this.height = height;
+	this.speedX = 0;
+	this.speedY = 0;
+	this.x = x;
+	this.y = y;
+	this.update = function() {
+		ctx = myGameArea.context;
+		if (this.type == "image") {
+			ctx.drawImage(this.image, 
+				this.x, 
+				this.y,
+				this.width, this.height);
+		} 
+		else {
+			ctx.fillStyle = color;
+			ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
+	this.newPos = function() {
+		this.x += this.speedX;
+		this.y += this.speedY;
+	}
+}
 
-	function updateGameArea() {
-		for (i = 0; i < tigers.length; i ++) {
-			if (myGamePiece.crashWith(tigers[i])) {
-				youLost();
-				myGameArea.stop();
-			} 
-		}
+function updateGameArea() {
+	myGameArea.clear();
+	myGameArea.frameNo += 1;
+	myBackground.newPos();
+	myBackground.update();
+	myScore.text="SCORE: " + myGameArea.frameNo;
+	myScore.update();
 
-		for (i = 0; i < dinos.length; i ++) {
-			if (myGamePiece.crashWith(dinos[i])) {
-				youLost();
-				myGameArea.stop();
-			} 
-		}
+	for (var i = 0; i < tigers.length; i ++) {
+		if (myGamePiece.crashWith(tigers[i])) {
+			youLost();
+			myGameArea.stop();
+		} 
+	}
 
-		for (i = 0; i < ghosts.length; i ++) {
-			if (myGamePiece.crashWith(ghosts[i])) {
-				youLost();
-				myGameArea.stop();
-			} 
-		}
+	for (var i = 0; i < dinos.length; i ++) {
+		if (myGamePiece.crashWith(dinos[i])) {
+			youLost();
+			myGameArea.stop();
+		} 
+	}
 
-		for (i = 0; i < cakes.length; i ++) {
-			if (myGamePiece.crashWith(cakes[i])) {
-				if(cakes.status === 1) {
-					cakes.status= 0;
-				}
-			} 
-		}
+	for (var i = 0; i < ghosts.length; i ++) {
+		if (myGamePiece.crashWith(ghosts[i])) {
+			youLost();
+			myGameArea.stop();
+		} 
+	}
 
-		myGameArea.clear();
-		myGameArea.frameNo += 1;
-		myBackground.newPos();
-		myBackground.update();
-		myScore.text="SCORE: " + myGameArea.frameNo;
-		myScore.update();
-
-		myGamePiece.speedX = 0;
-		myGamePiece.speedY = 0; 
-		if (myGameArea.keys && myGameArea.keys[37]) myGamePiece.speedX = -6; 
-		if (myGameArea.keys && myGameArea.keys[39]) myGamePiece.speedX = 6; 
-		if (myGameArea.keys && myGameArea.keys[38]) myGamePiece.speedY = -6; 
-		if (myGameArea.keys && myGameArea.keys[40]) myGamePiece.speedY = 6; 
-		if (myGamePiece.x <= 0) myGamePiece.speedX = 5;
-		if (myGamePiece.x >= 1175) myGamePiece.speedX = -5;
-		if (myGamePiece.y <= -5) myGamePiece.speedY = 5;
-		if (myGamePiece.y >= 675) myGamePiece.speedY = -5;
-		myGamePiece.newPos();
-		myGamePiece.update();
-
-		for( var i = 0; i < tigers.length; i++ ) {
-			if (tigers[i].x <= 0) {
-				tigers[i].speedX = tigers[i].speedX* -1 ;
+	for (var i = 0; i < cakes.length; i ++) {
+		if (myGamePiece.crashWith(cakes[i])) {
+			if(cakes[i].status === 1) {
+				myGameArea.frameNo += 200;
+				cakes[i].status = 0;
 			}
-			if (tigers[i].x >= 1155) {
-				tigers[i].speedX =  tigers[i].speedX* -1;
-			}
-			if (tigers[i].y <= -5) {
-				tigers[i].speedY = tigers[i].speedY* -1;
-			}
-			if (tigers[i].y >= 655) {
-				tigers[i].speedY = tigers[i].speedY* -1;
-			}
+		} 
+	}
+
+	myGamePiece.speedX = 0;
+	myGamePiece.speedY = 0; 
+	if (myGameArea.keys && myGameArea.keys[37]) myGamePiece.speedX = -6; 
+	if (myGameArea.keys && myGameArea.keys[39]) myGamePiece.speedX = 6; 
+	if (myGameArea.keys && myGameArea.keys[38]) myGamePiece.speedY = -6; 
+	if (myGameArea.keys && myGameArea.keys[40]) myGamePiece.speedY = 6; 
+	if (myGamePiece.x <= 0) myGamePiece.speedX = 5;
+	if (myGamePiece.x >= 1175) myGamePiece.speedX = -5;
+	if (myGamePiece.y <= -5) myGamePiece.speedY = 5;
+	if (myGamePiece.y >= 675) myGamePiece.speedY = -5;
+	myGamePiece.newPos();
+	myGamePiece.update();
+
+	for( var i = 0; i < tigers.length; i++ ) {
+		if (tigers[i].x <= 0) {
+			tigers[i].speedX = tigers[i].speedX* -1 ;
+		}
+		if (tigers[i].x >= 1155) {
+			tigers[i].speedX =  tigers[i].speedX* -1;
+		}
+		if (tigers[i].y <= -5) {
+			tigers[i].speedY = tigers[i].speedY* -1;
+		}
+		if (tigers[i].y >= 655) {
+			tigers[i].speedY = tigers[i].speedY* -1;
+		}
+		if(tigers[i].status === 1){
 			tigers[i].update();
 			tigers[i].newPos();
 		}
+	}
 
-		for( var i = 0; i < dinos.length; i++ ) {
-			if (dinos[i].x <= 0) {
-				dinos[i].speedX = dinos[i].speedX* -1 ;
-			}
-			if (dinos[i].x >= 1145) {
-				dinos[i].speedX =  dinos[i].speedX* -1;
-			}
-			if (dinos[i].y <= -5) {
-				dinos[i].speedY = dinos[i].speedY* -1;
-			}
-			if (dinos[i].y >= 645) {
-				dinos[i].speedY = dinos[i].speedY* -1;
-			}
+	for( var i = 0; i < dinos.length; i++ ) {
+		if (dinos[i].x <= 0) {
+			dinos[i].speedX = dinos[i].speedX* -1 ;
+		}
+		if (dinos[i].x >= 1145) {
+			dinos[i].speedX =  dinos[i].speedX* -1;
+		}
+		if (dinos[i].y <= -5) {
+			dinos[i].speedY = dinos[i].speedY* -1;
+		}
+		if (dinos[i].y >= 645) {
+			dinos[i].speedY = dinos[i].speedY* -1;
+		}
+		if(dinos[i].status === 1){
 			dinos[i].update();
 			dinos[i].newPos();
 		}
+	}
 
-		for( var i = 0; i < ghosts.length; i++ ) {
-			if (ghosts[i].x <= 0) {
-				ghosts[i].speedX = ghosts[i].speedX* -1 ;
-			}
-			if (ghosts[i].x >= 1145) {
-				ghosts[i].speedX =  ghosts[i].speedX* -1;
-			}
-			if (ghosts[i].y <= -5) {
-				ghosts[i].speedY = ghosts[i].speedY* -1;
-			}
-			if (ghosts[i].y >= 645) {
-				ghosts[i].speedY = ghosts[i].speedY* -1;
-			}
+	for( var i = 0; i < ghosts.length; i++ ) {
+		if (ghosts[i].x <= 0) {
+			ghosts[i].speedX = ghosts[i].speedX* -1 ;
+		}
+		if (ghosts[i].x >= 1145) {
+			ghosts[i].speedX =  ghosts[i].speedX* -1;
+		}
+		if (ghosts[i].y <= -5) {
+			ghosts[i].speedY = ghosts[i].speedY* -1;
+		}
+		if (ghosts[i].y >= 645) {
+			ghosts[i].speedY = ghosts[i].speedY* -1;
+		}
+		if(ghosts[i].status === 1){
 			ghosts[i].update();
 			ghosts[i].newPos();
 		}
-		
+	}
 
-		for( var i = 0; i < cakes.length; i++ ) {
-			console.log("cakes")
-			cakes[i] = { x: 0, y: 0, status: 1 };
-
-			console.log(cakes.status);
-			//cakes[i].update();
-			//cakes[i].newPos();
+	for (var i = 0; i < cakes.length; i ++) {
+		if(cakes[i].status === 1){
+			cakes[i].update();
+			cakes[i].newPos();
 		}
 	}
 
+}
 
-	function youLost() {
-		alert("You lost! Your scorce is: " + myGameArea.frameNo);
-	}
+
+function youLost() {
+	alert("You lost! Your scorce is: " + myGameArea.frameNo);
+}
